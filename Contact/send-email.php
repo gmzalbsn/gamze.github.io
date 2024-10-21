493a013da07c0caba338fff800b8c017c265307a
gmzalb <47821308+gmzalbsn@users.noreply.github.com>
2024-10-21 20:15:57 +0300

    Changed send-email.php

--- Contact/send-email.php
+++ Contact/send-email.php
@@ -1,43 +1,29 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require '../vendor/autoload.php';  
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-require 'Contact/PHPMailer/src/Exception.php';
-require 'Contact/PHPMailer/src/PHPMailer.php';
-require 'Contact/PHPMailer/src/SMTP.php';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];        
-    $email = $_POST['email'];      
-    $phone = $_POST['phone'];     
-    $message = $_POST['message'];  
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
 
-    $mail = new PHPMailer(true);
+    // The email address to send to
+    $to = "gamzealbsn@gmail.com";  // Replace with your email address
+    $subject = "New Contact Form Message";
 
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';        
-        $mail->SMTPAuth = true;                 
-        $mail->Username = $_ENV['GMAIL_USERNAME'];  
-        $mail->Password = $_ENV['GMAIL_PASSWORD'];  
-        $mail->SMTPSecure = 'tls';             
-        $mail->Port = 587;                    
+    // Email content
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Phone: $phone\n";
+    $email_message .= "Message: $message\n";
+    
+    // Email headers
+    $headers = "From: $email";
 
-        $mail->setFrom($_ENV['GMAIL_USERNAME'], 'Web Formu'); 
-        $mail->addAddress($_ENV['GMAIL_USERNAME']);          
-        $mail->isHTML(true);                                
-        $mail->Subject = 'Yeni İletişim Formu Mesajı';       
-        $mail->Body = "Ad: $name<br>E-posta: $email<br>Telefon: $phone<br>Mesaj: $message"; 
-
-        $mail->send();
-        echo 'Mesaj başarıyla gönderildi!';
-    } catch (Exception $e) {
-        echo "Mesaj gönderilemedi. Hata: {$mail->ErrorInfo}";
+    // Send the email
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo 'Your message was successfully sent!';
+    } else {
+        echo 'There was an issue sending your message. Please try again.';
     }
 }
-
 ?>
