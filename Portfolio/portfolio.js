92ef91071704d5cb7af4ee0e4d96bf0e590928ac
gmzalb <47821308+gmzalbsn@users.noreply.github.com>
2024-10-22 00:49:18 +0300

    Portfolio section deleted

--- Portfolio/portfolio.js
+++
@@ -1,6 +0,0 @@
-function openModal(projectId) {
-    document.getElementById(projectId).style.display = 'flex';
-}
-
-function closeModal(projectId) {
-    document.getElementById(projectId).style.display = 'none';
