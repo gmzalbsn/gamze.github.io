2af596fcabf1b3cdd51279fb94f99ce046e652d5
gmzalbsn <47821308+gmzalbsn@users.noreply.github.com>
2024-10-22 01:33:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll(".carousel-item");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${(index - slideIndex) * 100}%)`;
+    });
+}
