255aa9aec3b37c92d9b3effa2ee9930f3fb53b8b
gmzalb <47821308+gmzalbsn@users.noreply.github.com>
2024-10-22 00:13:44 +0300

    Added portfolio html and css files

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,6 @@
+function openModal(projectId) {
+    document.getElementById(projectId).style.display = 'flex';
+}
+
+function closeModal(projectId) {
+    document.getElementById(projectId).style.display = 'none';
