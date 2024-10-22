b185b17908e43148827796db0283a1e649664422
gmzalbsn <47821308+gmzalbsn@users.noreply.github.com>
2024-10-22 22:56:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -23,12 +23,7 @@ function plusSlides(n) {
 
 function showSlides(n) {
     let slides = document.querySelectorAll(".carousel-item");
-    if (n >= slides.length) {
-        slideIndex = 0;
-    }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    }
+    slideIndex = (n + slides.length) % slides.length;  // Ensure circular looping
     slides.forEach((slide, index) => {
         slide.style.transform = `translateX(${(index - slideIndex) * 100}%)`;
     });
